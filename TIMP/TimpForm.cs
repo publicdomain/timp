4ad9ca88fc49d2fa404516faa39264fadd635b83
Victor <publicdomainvic@gmail.com>
2024-01-13 00:12:57 -0400

    Add NAudioPlayNew()

--- TIMP/TimpForm.cs
+++ TIMP/TimpForm.cs
@@ -1,9 +1,12 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
+using NAudio.Wave;
+using System.Windows.Forms.VisualStyles;
 
 namespace TIMP
 {
@@ -22,6 +25,31 @@ namespace TIMP
         /// </summary>
         bool closeFlag = false;
 
+        /// <summary>
+        /// The output device.
+        /// </summary>
+        private WaveOutEvent outputDevice;
+
+        /// <summary>
+        /// The audio file.
+        /// </summary>
+        private AudioFileReader audioFile;
+
+        /// <summary>
+        /// Play back state.
+        /// </summary>
+        private enum PlayBackState
+        {
+            Stopped = 0,
+            Playing = 1,
+            Paused = 2,
+        }
+
+        /// <summary>
+        /// The random.
+        /// </summary>
+        private static Random random = new Random();
+        
         /// <summary>
         /// Initializes a new instance of the <see cref="T:TIMP.TimpForm"/> class.
         /// </summary>
@@ -92,7 +120,8 @@ namespace TIMP
         /// <param name="e">Event arguments.</param>
         private void OnPlayerListBoxSelectedIndexChanged(object sender, EventArgs e)
         {
-            // TODO Add code
+            // TODO Play selected one [May be improved]
+            this.NAudioPlayNew(Path.Combine(this.folderBrowserDialog.SelectedPath, this.playerListBox.Items[this.playerListBox.SelectedIndex].ToString()));
         }
 
         /// <summary>
@@ -117,10 +146,40 @@ namespace TIMP
                         this.playerListBox.Items.Clear();
 
                         // Set files by extension
-                        FileInfo[] files = new DirectoryInfo(this.folderBrowserDialog.SelectedPath).EnumerateFiles("*.*", SearchOption.AllDirectories).Where(file => new string[] { ".mp3", ".wav", ".aif" }.Contains(file.Extension.ToLower())).ToArray();
+                        FileInfo[] files = new DirectoryInfo(this.folderBrowserDialog.SelectedPath).EnumerateFiles("*.*", SearchOption.TopDirectoryOnly).Where(file => new string[] { ".mp3", ".wav", ".aif" }.Contains(file.Extension.ToLower())).ToArray();
 
-                        // Add to list box
-                        this.playerListBox.Items.AddRange(files);
+                        // Check there are files
+                        if (files.Length > 0)
+                        {
+                            // Get only file names
+                            List<string> filesList = files.Select(f => f.Name).ToList();
+
+                            // Check if must shuffle
+                            if(this.randomizeToolStripMenuItem.Checked)
+                            {
+                                // TODO Shuffle files list [Can be improved by System.Security.Cryptography's RNGCryptoServiceProvider]
+                                filesList.OrderBy(a => random.Next()).ToList();
+                            }
+
+                            // Add files to list box
+                            this.playerListBox.Items.AddRange(filesList.ToArray());
+
+                            // Check if must autoplay
+                            if (this.autoplayToolStripMenuItem.Checked)
+                            {
+                                try
+                                {
+                                    // Play first file by selected index change
+                                    this.playerListBox.SelectedIndex = -1; // Deselect current
+                                    this.playerListBox.SelectedIndex = 0; // Trigger selected change event
+                                }
+                                catch (Exception ex)
+                                {
+                                    // TODO Log to file
+                                    MessageBox.Show(ex.Message);
+                                }
+                            }
+                        }
                     }
 
                     // Halt flow
@@ -151,7 +210,87 @@ namespace TIMP
                     // Halt flow
                     break;
             }
+        }
 
+        /// <summary>
+        /// NAs the udio play new.
+        /// </summary>
+        /// <param name="audioFilePath">Audio file path.</param>
+        private void NAudioPlayNew(string audioFilePath)
+        {
+            // Check for a previous output device
+            if (outputDevice != null)
+            {
+                // Reset NAudio
+                this.NAudioReset();
+            }
+
+            // Set the output device
+            outputDevice = new WaveOutEvent();
+            outputDevice.PlaybackStopped += OnPlaybackStopped;
+
+            // Set the audio file and init
+            audioFile = new AudioFileReader(audioFilePath);
+            outputDevice.Init(audioFile);
+
+            // Play it
+            outputDevice.Play();
+        }
+
+        /// <summary>
+        /// NAs the udio play.
+        /// </summary>
+        private void NAudioPlay()
+        {
+            // TODO Reserved for play/pause
+        }
+
+        /// <summary>
+        /// NAs the udio play.
+        /// </summary>
+        private void NAudioPause()
+        {
+            // TODO Reserved for play/pause
+        }
+
+        /// <summary>
+        /// Stop NAudio playback.
+        /// </summary>
+        private void NAudioStop()
+        {
+            // Stop the output device
+            this.outputDevice?.Stop();
+        }
+
+        /// <summary>
+        /// Ons the playback stopped.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="args">Arguments.</param>
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+            // Dispose by reset
+            this.NAudioReset();
+        }
+
+        /// <summary>
+        /// NAs the udio reset.
+        /// </summary>
+        private void NAudioReset()
+        {
+            // Reset output device
+            if (this.outputDevice != null)
+            {
+                this.outputDevice.Dispose();
+                this.outputDevice = null;
+            }
+
+            // Reset audio file
+            if (this.audioFile != null)
+            {
+                this.audioFile.Dispose();
+                this.audioFile = null;
+            }
         }
     }
 }
