a84cf7b640f2ec356765b8b7015beb6c1c1978b2
Victor <publicdomainvic@gmail.com>
2024-01-27 12:57:09 -0400

    Add single instance by mutex

--- TIMP/Program.cs
+++ TIMP/Program.cs
@@ -1,23 +1,41 @@
-﻿
-using System;
+﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace TIMP
 {
-	/// <summary>
-	/// Class with program entry point.
-	/// </summary>
-	internal sealed class Program
-	{
-		/// <summary>
-		/// Program entry point.
-		/// </summary>
-		[STAThread]
-		private static void Main(string[] args)
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new TimpApplicationContext());
-		}		
-	}
+    /// <summary>
+    /// Class with program entry point.
+    /// </summary>
+    internal sealed class Program
+    {
+        /// <summary>
+        /// The mutex.
+        /// </summary>
+        private static Mutex mutex = null;
+
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            // The mutex flag
+            bool mutexFlag;
+
+            // Set the mutex
+            mutex = new Mutex(true, "timpMutex", out mutexFlag);
+
+            // Check the mutex flag
+            if (!mutexFlag)
+            {
+                // There is an instance of TIMP running, exit
+                return;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TimpApplicationContext());
+        }
+    }
 }
